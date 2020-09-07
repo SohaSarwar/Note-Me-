cda52a7e54ef775d4580cecb37718d6279f74abb
SohaSarwar <68748330+SohaSarwar@users.noreply.github.com>
2020-09-08 00:59:37 +0500

    Note Me App

---
+++ Note-Tracking/app/src/main/java/com/xstudioo/noteme/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.xstudioo.noteme;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager
+                .LayoutParams.FLAG_FULLSCREEN);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },SPLASH_SCREEN);
+
+
+
+
+    }
+}
